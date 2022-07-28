bfc61d1f101d00c8aee5a9a789b04ebcf19bf72f
naveenpremise <naveen.kumar.c@premise.com>
2022-07-28 19:07:19 +0530

    Added input nutton for login page

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default function Login() {
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('')
   return (
-    <div>Login</div>
+    <div>
+      <input
+        type='text'
+        onChange={(e)=>setName(e.target.value)}/>
+      <input
+        type='text'
+        onChange={(e) => setPassword(e.target.value)} />
+      <button disabled = {(name&&password)?false:true}>click</button>
+    </div>
   )
 }
