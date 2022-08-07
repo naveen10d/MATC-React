28e770fd9b651c52f563451187cb660988936b47
naveenpremise <naveen.kumar.c@premise.com>
2022-08-07 20:24:27 +0530

    Added image in Login and Register

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Routers from './Components/Routers';
+// import Routers from './Components/Routers';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Login from './Components/Login';
