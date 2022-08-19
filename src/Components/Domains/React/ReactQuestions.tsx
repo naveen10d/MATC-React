29ee8eb544b9917b7a743d4416560ddb42801fc2
Naveen10D <naveenkumar@10decoders.in>
2022-08-19 20:01:02 +0530

    Added questions in react page

--- src/Components/Domains/React/ReactQuestions.tsx
+++ src/Components/Domains/React/ReactQuestions.tsx
@@ -2,24 +2,26 @@ import { useState, useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../../Redux/ReduxHooks'
 import { getQuestions } from '../../../Redux/action/QuestionAction'
 interface Questions {
-    question: String,
-    options: Array<String>,
-    correctAnswer: String,
-    questionType: String
+    question: string,
+    options: Array<string>,
+    correctAnswer: string,
+    questionType: string
 }
 export default function ReactQuestions() {
     const [questions, setQuestions] = useState<Questions[]>([])
-    const questionsssss = useAppSelector(state => state.questions)
+    const [index, setIndex] = useState<number>(0)
+    const reactQuestions = useAppSelector(state => state.questions)
     const dispatch = useAppDispatch()
     useEffect(() => {
-        dispatch(getQuestions())
-        setQuestions(questionsssss)
+        dispatch(getQuestions(`/question?questionType=react`))
+        setQuestions(reactQuestions)
     }, [])
 
-    console.log("questionsssss===>>>>", questions)
-
-    const handleChangeChk = () => {
-        console.log('dddddddddddddd')
+    const handleClick = () => {
+        setIndex(index + 1)
+    }
+    const handleChange = () => {
+        console.log("first")
     }
     return (
         <div className='container'>
@@ -27,29 +29,20 @@ export default function ReactQuestions() {
                 <div className='col-12'>
                     <div className='d-flex flex-column justify-content-center align-items-center shadow text-muted m-4'>
                         <div className='d-flex justify-content-center align-items-center fs-1 m-5 text-dark'> React Questions</div>
-                        <div>
-                            <div>
-                                {
-                                    questions && questions.map((item: Questions) => {
-                                        return 'ddd'
-                                        console.log(item)
-                                    })
-                                }
-
-                            </div>
-                            {/* <p>{questions.question}</p>
-                            <div className='d-flex flex-column align-self-start'>
-                                {questions.options.forEach((option: any) =>
+                        <div>{questions.length > 1 &&
+                            <p>{questions[index].question}</p>}
+                            {/* <div className='d-flex flex-column align-self-start'>
+                                {questions[index].options.map((option: any) =>
                                     <label>
-                                        <input type="checkbox" onChange={handleChangeChk} />
+                                        <input type="checkbox" onChange={handleChange} />
                                         <span className='p-3'>{option}</span>
                                     </label>
-                                )} */}
-                            {/* </div> */}
+                                )}
+                            </div> */}
                         </div>
                         <div className='d-flex justify-content-between px-5 w-75 m-5'>
                             <button>Previous</button>
-                            <button>Next</button>
+                            <button onClick={handleClick}>Next</button>
                         </div>
                     </div>
                 </div>
--- src/Redux/action/QuestionAction.ts
+++ src/Redux/action/QuestionAction.ts
@@ -2,14 +2,15 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:4000'
 
-export const getQuestions = () => async (dispatch: any) => {
+export const getQuestions = (url: String) => async (dispatch: any) => {
+    let apiUrl = `${baseUrl}${url}`
     try {
-        await axios.get(`${baseUrl}/question`).then((response) =>
+        await axios.get(apiUrl).then((response) => {
             dispatch({
                 type: 'GET',
                 payload: response.data
             })
-        )
+        })
     }
     catch (error) {
         return error
